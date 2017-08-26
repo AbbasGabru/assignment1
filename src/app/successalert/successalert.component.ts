58932af3736d0b43d719d8a7be01f26db85d983c
AbbasGabru <abbasgabru52@gmail.com>
2017-08-26 16:13:39 +0530

    my first app

---
+++ src/app/successalert/successalert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-successalert',
+    template:`<p>I'm in SuccessAlert Component.</p>`,
+    styles:[`p{color:green}`]
+})
+export class SuccessAlertComponent{
+}
---
+++ src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-warningalert',
+    template:`<p>I'm in WarningAlert Component.</p>
+    <app-successalert></app-successalert>
+    `,
+    styleUrls:['./warningalert.component.css']
+})
+export class WarningAlertComponent{
+    
+}

1c98b9bb77d63d84f21dd1f082c0caf1e0e2c647
AbbasGabru <abbasgabru52@gmail.com>
2017-08-27 16:23:09 +0530

    after removal of all files

--- src/app/successalert/successalert.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector:'app-successalert',
-    template:`<p>I'm in SuccessAlert Component.</p>`,
-    styles:[`p{color:green}`]
-})
-export class SuccessAlertComponent{
-}
